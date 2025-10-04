6adc948ff554fa657c5ffa746725641346a3ce5d
MJ Deus <mdeus@usc.edu>
2025-10-03 18:51:41 -0700

    setIntersection and setUnion

--- util.h
+++ util.h
@@ -13,20 +13,30 @@
 template <typename T>
 std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
 {
+    std::set<T> result; // need something to store our output
 
+    // now instead of an O(nlogn) solution, we can do this in O(n) time, but im feeling too lazy to actually implement that tonight (my bad)
 
+    // go through each element in s1, and check if it's in s2
+    for(const auto& elt : s1) {
+        // if it's in s2, add it to the result set
+        if(s2.find(elt) != s2.end()) {
+            result.insert(elt);
+        }
+    }
 
-
-
+    return result;
 }
 template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
 {
+    std::set<T> result = s1; // start with LHS 
 
+    // s1 starts off result, so we can just copy the rest of s2 into result
+    result.insert(s2.begin(), s2.end());
 
-
-
-
+    // return the combined sets (union)
+    return result;
 }
 
 /***********************************************/
